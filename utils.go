e4a7eab82631ee3c8d31c9d2966d433459d6875f
Jon Nappi <jnappi@dyn.com>
2016-07-19 11:21:59 -0400

    consolidate utils

--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package qstring
 
 import (
 	"reflect"
+	"strings"
 	"time"
 )
 
@@ -29,4 +30,17 @@ func isEmptyValue(v reflect.Value) bool {
 		}
 	}
 	return false
+}
+
+// parseTag splits a struct field's qstring tag into its name and, if an
+// optional omitempty option was provided, a boolean indicating this is
+// returned
+func parseTag(tag string) (string, bool) {
+	if idx := strings.Index(tag, ","); idx != -1 {
+		if tag[idx+1:] == "omitempty" {
+			return tag[:idx], true
+		}
+		return tag[:idx], false
+	}
+	return tag, false
 }
--- utils_test.go
+++ utils_test.go
@@ -43,3 +43,55 @@ func TestIsEmptyValue(t *testing.T) {
 		}
 	}
 }
+
+func TestTagParsing(t *testing.T) {
+	testio := []struct {
+		inp    string
+		output string
+		omit   bool
+	}{
+		{inp: "name,omitempty", output: "name", omit: true},
+		{inp: "name", output: "name", omit: false},
+		{inp: "name,", output: "name", omit: false},
+		{inp: "name", output: "name", omit: false},
+		{inp: "", output: "", omit: false},
+		{inp: ",omitempty", output: "", omit: true},
+		{inp: "-", output: "-", omit: false},
+	}
+
+	var name string
+	var omit bool
+	for _, test := range testio {
+		name, omit = parseTag(test.inp)
+		if name != test.output {
+			t.Errorf("Expected tag name to be %q, got %q instead", test.output, name)
+		}
+
+		if omit != test.omit {
+			t.Errorf("Expected omitempty to be %t, got %t instead", test.omit, omit)
+		}
+	}
+}
+
+func TestOmitEmpty(t *testing.T) {
+	vis := 5
+	testio := []struct {
+		Visible     int
+		Conditional int `qstring:"conditional,omitempty"`
+		omit        bool
+	}{
+		{Visible: vis, Conditional: 5, omit: false},
+		{Visible: vis, Conditional: 0, omit: true},
+	}
+
+	for _, test := range testio {
+		values, _ := MarshalValues(&test)
+
+		_, found := values["conditional"]
+		if found && test.omit {
+			t.Errorf("%d was unexpectedly marshaled", test.Conditional)
+		} else if !found && !test.omit {
+			t.Errorf("%d was not marshaled, but should have been", test.Conditional)
+		}
+	}
+}
